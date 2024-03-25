597119ff3599b86f9a4be5d5b35c704423f9e362
Fernando Sousa ∵ F.R.C. <fernando@sousafrc.com>
2024-03-24 23:43:52 -0300

    :sparkles: feat: Copyright fixo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center gap-2">
+    <div className="flex flex-col items-center gap-2 pb-5">
       <h3 className="text-3xl font-bold text-slate-100">Cipher Key</h3>
       <div className="text-center">
         <p className="text-slate-400">
           Copyright © 2024 <b>Cipher Key</b>
         </p>
-        <p className="text-slate-400">Desenvolvido com ❤️ por <strong>Fernando Sousa ∵ F.R.C.</strong></p>
+        <p className="text-slate-400">
+          Desenvolvido e mantido com ❤️ por{" "}
+          <a
+            href="https://github.com/sousafrc"
+            className="cursor-pointer font-bold"
+          >
+            Fernando Sousa ∵ F.R.C.
+          </a>
+        </p>
       </div>
     </div>
   );
